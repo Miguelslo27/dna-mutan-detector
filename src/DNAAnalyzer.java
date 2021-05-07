38df05cf995645e525b38b13a4401d2214898941
Miguel Sosa <miguelmail2006@gmail.com>
2021-05-07 01:37:10 -0300

    Changes the first approach to improve the outcome

--- src/DNAAnalyzer.java
+++ src/DNAAnalyzer.java
@@ -3,8 +3,9 @@ import java.util.Arrays;
 public class DNAAnalyzer {
     private static int moleculeSize = 0;
 
-    public static void isMutant(String[] dnaMolecule) throws Exception {
+    public static boolean isMutant(String[] dnaMolecule) throws Exception {
         boolean isMutantDNA = false;
+
         System.out.printf("There are %d chains in the DNA Molecule%n", Arrays.stream(dnaMolecule).count());
 
         if (!isValidDNA(dnaMolecule)) {
@@ -13,47 +14,53 @@ public class DNAAnalyzer {
 
         System.out.println("The DNA molecule is valid");
 
-        int basesCountInARow = 0;
-        int basesCountInAColumn = 0;
-        int basesCountInEastDiagonal = 0;
-        int basesCountInWestDiagonal = 0;
-
         // Check if is actually a mutant
         for (int c = 0; c < Arrays.stream(dnaMolecule).count(); c++) {
             System.out.printf("Chain %s%n", dnaMolecule[c]);
 
+            int basesCountInARow = 0;
+            int basesCountInAColumn = 0;
+            int basesCountInEastDiagonal = 0;
+            int basesCountInWestDiagonal = 0;
             String chain = dnaMolecule[c];
 
-            // Check the base with the next in the row
             for (int b = 0; b < chain.length(); b++) {
-                System.out.println(chain.toCharArray()[b]);
+                // Check the base with the next in the row
+                System.out.printf("Base %s%n", chain.toCharArray()[b]);
 
-                if (b + 1 >= chain.length() || b + 2 >= chain.length() || b + 3 >= chain.length()) {
-                    System.out.println("====================");
+                String base = Character.toString(chain.toCharArray()[b]);
+
+                // Test the column
+                if (b + 3 == chain.length()) {
+                    // This is out of row scope, so can brake the loop here
                     break;
                 }
 
-                System.out.println(chain.toCharArray()[b + 1]);
-                System.out.println(chain.toCharArray()[b + 2]);
-                System.out.println(chain.toCharArray()[b + 3]);
+                String baseOneStepNext = Character.toString(chain.toCharArray()[b + 1]);
+                String baseTwoStepsNext = Character.toString(chain.toCharArray()[b + 2]);
+                String baseThreeStepsNext = Character.toString(chain.toCharArray()[b + 3]);
 
-                if (
-                    chain.toCharArray()[b] == chain.toCharArray()[b + 1]
-                    && chain.toCharArray()[b] == chain.toCharArray()[b + 2]
-                    && chain.toCharArray()[b] == chain.toCharArray()[b + 3]
-                ) {
-                    isMutantDNA = true;
+                if (c + 3 == Arrays.stream(dnaMolecule).count()) {
+                    // this is out of the column scope, so can brake the loop here
                     break;
                 }
 
-                System.out.println("====================");
+                String baseOneStepBelow = Character.toString(dnaMolecule[c + 1].toCharArray()[b]);
+                String baseTwoStepsBelow = Character.toString(dnaMolecule[c + 2].toCharArray()[b]);
+                String baseThreeStepsBelow = Character.toString(dnaMolecule[c + 3].toCharArray()[b]);
+
+                String baseOneStepDiagonalEast = Character.toString(dnaMolecule[c + 1].toCharArray()[b + 1]);
+                String baseTwoStepDiagonalEast = Character.toString(dnaMolecule[c + 2].toCharArray()[b + 2]);
+                String baseThreeStepDiagonalEast = Character.toString(dnaMolecule[c + 2].toCharArray()[b + 3]);
+
+                //if (base == chain.toCharArray()[b])
             }
 
+            System.out.println("===");
             if (isMutantDNA) break;
-
-            // Check the base with the next in the column
-            // Check the base with the next in the diagonal east
         }
+
+        return isMutantDNA;
     }
 
     public static boolean isValidDNA(String[] dnaMolecule) {
